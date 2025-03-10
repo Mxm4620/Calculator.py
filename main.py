12d9a0fe33bcc119d1560dc6797338628c63f924
Mxm4620 <maximstep2407@gmail.com>
2025-03-10 21:10:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from tkinter import *
 
+count = ""
+
+def submit():
+    username = entry.get()
+    print(username)
+
+def click(num):
+    entry.insert(Tk.END, num)
+
 window = Tk()
 
+entry = Entry(window,bg="black")
+entry.pack()
+
+button_submit = Button(window,text="=",command=submit)
+button_submit.pack()
+
+button_1 = Button(window,text="1",command=lambda: 1)
+button_1.pack(side=LEFT)
 window.mainloop()
