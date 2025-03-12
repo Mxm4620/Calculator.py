c4394cd91ef8185f0b4418704dbdaf36b7628571
Mxm4620 <maximstep2407@gmail.com>
2025-03-12 20:21:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import math
 
 number_1 = input("Press the first number:")
 number_2 = input("Press the second number:")
-sign = input("Put right sing what you need in your math analys(+ - * /):")
+sign = input("Put right sign what you need in your math analys(+ - * /):")
 result_plus = int(number_1) + int(number_2)
 result_minus = int(number_1) - int(number_2)
 result_multiply = int(number_1) * int(number_2)
